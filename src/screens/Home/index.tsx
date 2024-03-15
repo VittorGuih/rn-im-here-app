f58cf97b9e2fc80a57f003ce0bf857d634445ea3
VittorGuih <vittytuuguilherme@gmail.com>
2024-03-15 05:14:30 -0300

    cria formulario para adicionar novo participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./style";
 
 export function Home() {
+  function handleParticipantAdd() {
+    return
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -13,11 +17,19 @@ export function Home() {
         Sexta, 15 de Março de 2024.
       </Text>
 
-      <TextInput 
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
