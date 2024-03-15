f5fbb93070b88348e3cc86aded58f13b78d633a5
VittorGuih <vittytuuguilherme@gmail.com>
2024-03-15 05:25:34 -0300

    cria componente de participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./style";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
   function handleParticipantAdd() {
@@ -30,6 +31,7 @@ export function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+      <Participant />
     </View>
   )
 }
